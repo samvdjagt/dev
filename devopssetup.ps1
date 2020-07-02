b3d6460696c7791ec1b8d2f27623dbb8e610b8e1
samvdjagt <52912346+samvdjagt@users.noreply.github.com>
2020-07-02 12:25:29 -0600

    Update devopssetup.ps1

--- devopssetup.ps1
+++ devopssetup.ps1
@@ -121,12 +121,12 @@ write-output $body
 $response = Invoke-RestMethod -Uri $url -Headers @{Authorization = "Basic $token"} -Method Post -Body $Body -ContentType application/json
 write-output $response
 
-$url= $("https://dev.azure.com/" + $orgName + "/_apis/git/repositories?api-version=5.1")
-write-output $url
-
 $url = $("https://dev.azure.com/" + $orgName + "/_apis/projects/" + $projectName + "api-version=5.1")
 $response = Invoke-RestMethod -Uri $url -Headers @{Authorization = "Basic $token"} -Method Get
 $projectId = $response.id
+
+$url= $("https://dev.azure.com/" + $orgName + "/_apis/git/repositories?api-version=5.1")
+write-output $url
 
 $body = @"
 {
