9fb5c125a9f1c98f170a71b8a8b8687116e3bdc5
samvdjagt <52912346+samvdjagt@users.noreply.github.com>
2020-07-06 14:53:29 -0600

    Update componentssetup.ps1

--- componentssetup.ps1
+++ componentssetup.ps1
@@ -23,75 +23,89 @@ Invoke-StorageAccountPostDeployment -orchestrationFunctionsPath $currentDir -sto
 
 Upload any required data to the storage account
 #>
-function Invoke-StorageAccountPostDeployment {
 
-    [CmdletBinding(SupportsShouldProcess = $true)]
-    param(
-        [Parameter(Mandatory = $true)]
-        [string] $orchestrationFunctionsPath,
+    #Initializing variables
+$SubscriptionId = Get-AutomationVariable -Name 'subscriptionid'
+$ResourceGroupName = Get-AutomationVariable -Name 'ResourceGroupName'
+$fileURI = Get-AutomationVariable -Name 'fileURI'
+$AutomationAccountName = Get-AutomationVariable -Name 'AccountName'
+$AppName = Get-AutomationVariable -Name 'AppName'
+$principalId = Get-AutomationVariable -Name 'principalId'
+$orgName = Get-AutomationVariable -Name 'orgName'
+$projectName = Get-AutomationVariable -Name 'projectName'
+$storageAccountName = Get-AutomationVariable -Name 'componentsStorage'
 
-        [Parameter(Mandatory = $true)]
-        [string] $wvdUploadsPath,
 
-        [Parameter(Mandatory = $true)]
-        [string] $storageAccountName
-    )
+$FileNames = "msft-wvd-saas-api.zip,msft-wvd-saas-web.zip,AzureModules.zip,modules.zip"
+$SplitFilenames = $FileNames.split(",")
+foreach($Filename in $SplitFilenames){
+Invoke-WebRequest -Uri $fileURI/$Filename -OutFile "C:\$Filename"
+}
 
-    begin {
+#New-Item -Path "C:\msft-wvd-saas-offering" -ItemType directory -Force -ErrorAction SilentlyContinue
+Expand-Archive "C:\AzureModules.zip" -DestinationPath 'C:\Modules\Global' -ErrorAction SilentlyContinue
+
+Import-Module Az.Accounts -Global
+Import-Module Az.Resources -Global
+Import-Module Az.Websites -Global
+Import-Module Az.Automation -Global
+Import-Module AzureAD -Global
+
+Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope Process -Force -Confirm:$false
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force -Confirm:$false
+Get-ExecutionPolicy -List
+
+#The name of the Automation Credential Asset this runbook will use to authenticate to Azure.
+$CredentialAssetName = 'ServicePrincipalCred'
+
+#Authenticate Azure
+#Get the credential with the above name from the Automation Asset store
+$PSCredentials = Get-AutomationPSCredential -Name $CredentialAssetName
+
+
+#The name of the Automation Credential Asset this runbook will use to authenticate to Azure.
+$CredentialAssetName2 = 'ManagementUXDeploy'
+
+#Authenticate Azure
+#Get the credential with the above name from the Automation Asset store
+$AzCredentials = Get-AutomationPSCredential -Name $CredentialAssetName2
+$AzCredentials.password.MakeReadOnly()
+$username = $AzCredentials.username
+Connect-AzAccount -Environment 'AzureCloud' -Credential $AzCredentials
+Connect-AzureAD -AzureEnvironmentName 'AzureCloud' -Credential $AzCredentials
+Select-AzSubscription -SubscriptionId $SubscriptionId
+
+# Get the context
+$context = Get-AzContext
+if ($context -eq $null)
+{
+	Write-Error "Please authenticate to Azure & Azure AD using Login-AzAccount and Connect-AzureAD cmdlets and then run this script"
+	exit
+}
+
+    
+$tenant = (Get-AzTenant).TenantId
+$azureRmProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+$profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azureRmProfile)
+$pat = $profileClient.AcquireAccessToken($context.Subscription.TenantId).AccessToken
+$headers = @{    Authorization="Bearer $pat"}
+
+$token = $pat
+$token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($token)"))
+
+
+
         Write-Verbose ("[{0} entered]" -f $MyInvocation.MyCommand)
         . "$orchestrationFunctionsPath\Storage\Import-WVDSoftware.ps1"
         . "$orchestrationFunctionsPath\Storage\Compress-WVDCSEContent.ps1"
         . "$orchestrationFunctionsPath\Storage\Export-WVDCSEContentToBlob.ps1"
-    }
 
-    process {
 
-        Write-Verbose "###########################################"
-        Write-Verbose "## 1 - Download software from public url ##"
-        Write-Verbose "###########################################"
 
-        Write-Verbose("#####################")
-        Write-Verbose("## 1.1 - LOAD DATA ##")
-        Write-Verbose("#####################")
-        $ConfigurationFilePath = (Join-Path "$wvdUploadsPath/WVDScripts" "downloads.parameters.json")
-        $ConfigurationJson = Get-Content -Path $ConfigurationFilePath -Raw -ErrorAction 'Stop'
 
-        try { $Downloads = $ConfigurationJson | ConvertFrom-Json -ErrorAction 'Stop' }
-        catch {
-            Write-Error "Configuration JSON content could not be converted to a PowerShell object" -ErrorAction 'Stop'
-        }
-
-        Write-Verbose("####################")
-        Write-Verbose("## 1.2 - EVALUATE ##")
-        Write-Verbose("####################")
-        foreach ($download in $Downloads.WVDSoftware) {
-            $InputObject = @{
-                Url  = $download.Url
-                FileName = (Join-Path "$wvdUploadsPath/WVDScripts" $download.DestinationFilePath)
-            }
-            Write-Verbose $InputObject
-
-            if ($PSCmdlet.ShouldProcess("Required executable files to be installed on WVD VMs", "Import")) {
-                Import-WVDSoftware @InputObject -Verbose
-                Write-Verbose "WVD Software download invocation finished"
-            }
-        }
-        Write-Verbose "######################################################################################################"
-        Write-Verbose "## 2 - Create zip files for all WVDScripts subfolders and save them to the WVDCSEZipToUpload folder ##"
-        Write-Verbose "######################################################################################################"
-
-        $InputObject = @{
-            SourceFolderPath  = "$wvdUploadsPath/WVDScripts"
-            DestinationFolderPath = "$wvdUploadsPath/WVDCSEZipToUpload"
-        }
-        if ($PSCmdlet.ShouldProcess("$wvdUploadsPath/WVDScripts subfolders as .zip and store them into $wvdUploadsPath/WVDCSEZipToUpload", "Compress")) {
-            Compress-WVDCSEContent @InputObject -Verbose
-            Write-Verbose "WVD CSE for VMs compression finished"
-        }
-
-        Write-Verbose "###################################"
-        Write-Verbose "## 3 - Upload to storage account ##"
-        Write-Verbose "###################################"
+        Write-Verbose "###############################"
+        Write-Verbose "## Upload to storage account ##"
+        Write-Verbose "###############################"
 
         $InputObject = @{
             ResourceGroupName  = (Get-AzResource -Name $storageAccountName -ResourceType 'Microsoft.Storage/storageAccounts').ResourceGroupName
@@ -102,8 +116,63 @@ function Invoke-StorageAccountPostDeployment {
             Write-Verbose "Storage account content upload invocation finished"
         }
 
-    }
-    end {
-        Write-Verbose ("[{0} existed]" -f $MyInvocation.MyCommand)
+function Export-WVDCSEContentToBlob {
+
+    [CmdletBinding(SupportsShouldProcess = $True)]
+    param(
+        [Parameter(
+            Mandatory = $false,
+            HelpMessage = "Map of source/target tuples for upload"
+        )]
+        [Hashtable[]] $contentToUpload = $(
+            @{
+                sourcePath = 'C:\Pipeline\'
+                targetBlob = 'components'
+            }
+        )
+    )
+
+    Write-Verbose "Getting storage account context."
+    $storageAccount = Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName -ErrorAction Stop
+    $ctx = $storageAccount.Context
+
+    Write-Verbose "Building paths to the local folders to upload."
+    Write-Verbose "Script Directory: '$PSScriptRoot'"
+    $sourcesPath = Split-Path (Split-Path (Split-Path (Split-Path (Split-Path (Split-Path $PSScriptRoot -Parent) -Parent) -Parent) -Parent) -Parent) -Parent
+    $contentDirectory = Join-Path -Path $sourcesPath "parameters/s/Implementation/2020-Spring/OrchestrationSources/Uploads"
+    Write-Verbose "Content directory: '$contentDirectory'"
+
+    foreach ($contentObject in $contentToUpload) {
+
+        $sourcePath = $contentObject.sourcePath
+        $targetBlob = $contentObject.targetBlob
+
+        try {
+            $pathToContentToUpload = $sourcePath
+            Write-Verbose "Processing content in path: '$pathToContentToUpload'"
+    
+            Write-Verbose "Testing local path"
+            If (-Not (Test-Path -Path $pathToContentToUpload)) {
+                throw "Testing local paths FAILED: Cannot find content path to upload '$pathToContentToUpload'"
+            }
+            Write-Verbose "Testing paths: SUCCEEDED"
+    
+            Write-Verbose "Getting files to be uploaded..."
+            $scriptsToUpload = Get-ChildItem -Path $pathToContentToUpload -ErrorAction Stop
+            Write-Verbose "Files to be uploaded:"
+            Write-Verbose ($scriptsToUpload.Name | Format-List | Out-String)
+
+            Write-Verbose "Testing blob container"
+            Get-AzStorageContainer -Name $targetBlob -Context $ctx -ErrorAction Stop
+            Write-Verbose "Testing blob container SUCCEEDED"
+    
+            if ($PSCmdlet.ShouldProcess("Files to the '$targetBlob' container", "Upload")) {
+                $scriptsToUpload | Set-AzStorageBlobContent -Container $targetBlob -Context $ctx -Force -ErrorAction Stop
+            }
+        }
+        catch {
+            Write-Error "Upload FAILED: $_"
+        }
     }
 }
+
