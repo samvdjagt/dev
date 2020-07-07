6d264cff08a40841bafe1e2380fa6b3b0242f77e
samvdjagt <52912346+samvdjagt@users.noreply.github.com>
2020-07-07 11:45:36 -0600

    Update createDevopsPipeline.sh

--- createDevopsPipeline.sh
+++ createDevopsPipeline.sh
@@ -5,4 +5,4 @@
 
 az extension add --name azure-devops
 az login -u ${3} -p ${4}
-az pipelines create --name "WVD Quickstart" --organization "https://dev.azure.com/${1}" --project ${2} --repository ${2} --repository-type "tfsgit" --yml-path "pipeline.yml"
+az pipelines create --name "WVD Quickstart" --organization "https://dev.azure.com/${1}" --project ${2} --repository ${2} --repository-type "tfsgit" --branch "master" --yml-path "pipeline.yml"
