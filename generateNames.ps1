17ac1b2fc3d4c6fd541745eab98d72af3b2f4a31
samvdjagt <52912346+samvdjagt@users.noreply.github.com>
2020-07-08 10:15:41 -0600

    Delete generateNames.ps1

--- generateNames.ps1
+++
@@ -1,6 +0,0 @@
-$timeInt = (Get-date).TofileTime()
-$DeploymentScriptOutputs = @{}
-$DeploymentScriptOutputs['keyvaultName'] = \"keyvlt\" + $timeInt
-$DeploymentScriptOutputs['componentsName'] = \"comp\" + $timeInt
-$DeploymentScriptOutputs['assetsName'] = \"asset\" + $timeInt
-$DeploymentScriptOutputs['profilesName'] = \"profil\" + $timeInt
