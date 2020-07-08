c63c505ab4e2965ea0d45374d9944cb3b3e4d1d1
samvdjagt <52912346+samvdjagt@users.noreply.github.com>
2020-07-08 16:03:58 -0600

    Update Export-WVDCSEContentToBlob.ps1

--- SharedDeploymentFunctions/Storage/Export-WVDCSEContentToBlob.ps1
+++ SharedDeploymentFunctions/Storage/Export-WVDCSEContentToBlob.ps1
@@ -72,8 +72,8 @@ function Export-WVDCSEContentToBlob {
 
     Write-Verbose "Building paths to the local folders to upload."
     Write-Verbose "Script Directory: '$PSScriptRoot'"
-    $sourcesPath = Split-Path (Split-Path $PSScriptRoot -Parent) -Parent
-    $contentDirectory = Join-Path -Path $sourcesPath "Uploads"
+    $sourcesPath = Split-Path (Split-Path (Split-Path $PSScriptRoot -Parent) -Parent) -Parent
+    $contentDirectory = Join-Path -Path $sourcesPath "parameters/s/Uploads"
     Write-Verbose "Content directory: '$contentDirectory'"
 
     foreach ($contentObject in $contentToUpload) {
