82200dbb11cfdc06f392af5d912e9c41d003138c
Samuel Van Der Jagt <t-savand@microsoft.com>
2020-07-13 11:42:19 -0600

    check azure credentials

--- ARMRunbookScripts/checkAzureCredentials.ps1
+++ ARMRunbookScripts/checkAzureCredentials.ps1
@@ -1,9 +1,9 @@
 param(
 	[string] [Parameter(Mandatory=$true)] $username,
-	[System.Security.SecureString] [Parameter(Mandatory=$true)] $password
+	[string] [Parameter(Mandatory=$true)] $password
 )
 
 $ErrorActionPreference = 'Stop'
 
-$Credential = New-Object System.Management.Automation.PsCredential($username, $password)
+$Credential = New-Object System.Management.Automation.PsCredential($username, (ConvertTo-SecureString $password -AsPlainText -Force))
 Connect-AzAccount -credential $Credential
