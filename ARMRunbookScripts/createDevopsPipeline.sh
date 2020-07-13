bf489bc76fe5f6ed41b2dd1e790022f920369f9a
Samuel Van Der Jagt <t-savand@microsoft.com>
2020-07-13 11:59:20 -0600

    fix check azure cred

--- ARMRunbookScripts/createDevopsPipeline.sh
+++ ARMRunbookScripts/createDevopsPipeline.sh
@@ -5,8 +5,13 @@
 #     [Required]  ${1}  <orgName>        
 #     [Required]  ${2}  <projectName>         
 #     [Required]  ${3}  <azureAdminUpn>             
-#     [Required]  ${4}  <azureAdminPassword>   
+#     [Required]  ${4}  <azureAdminPassword>
+#     [Required]  ${5}  <createPipeline>   
 
 az extension add --name azure-devops
 az login -u ${3} -p ${4}
-az pipelines create --name "WVD Quickstart" --organization "https://dev.azure.com/${1}" --project ${2} --repository ${2} --repository-type "tfsgit" --branch "master" --yml-path "QS-WVD/pipeline.yml"
+
+if (${5} -eq 'true') {
+    az pipelines create --name "WVD Quickstart" --organization "https://dev.azure.com/${1}" --project ${2} --repository ${2} --repository-type "tfsgit" --branch "master" --yml-path "QS-WVD/pipeline.yml"
+}
+
